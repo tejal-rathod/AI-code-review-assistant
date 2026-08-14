867b6dd11c218a24553e026cb29bfdf16ed4abe0
tejal-rathod <tejal.rathod.023@gmail.com>
2026-08-14 17:52:45 +0530

    test file added

---
+++ src/TestVulnPR.java
@@ -0,0 +1,6 @@
+public class TestVulnPR {
+    private static final String SECRET = "admin_password_123";
+    public void query(String id) {
+        String sql = "SELECT * FROM users WHERE id='" + id + "'";
+    }
+}
